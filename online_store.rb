b2d8587720d0521c2e93d05ea6f9e9a4d694640b
codeamac09 <codeamac09@gmail.com>
2016-04-08 21:32:40 -0500

    initialize branch controller, Class OnlineStore initialize

--- online_store.rb
+++ online_store.rb
@@ -5,21 +5,65 @@ class OnlineStore
 
   def initialize
     @view = View.new
-    @user = User.new
+    @csv = CsvFiles.new
+    @products = @csv.read_products
+    @users = @csv.read_users
     master
   end#initialize
 
   def master
     option = gets.chomp
-    case option
-    when "1"    then @user.login
-    when "2" then @user.new_user
+    case option 
+    when "1" then aux = @view.login
+    when "2" then register
     else
       puts "Come back soon!"
     end
   end#mtd master
 
+  def register
+    aux = @view.register
+    @user = User.new(aux["type_user"], aux["email"], aux["password"])
+    @csv.add_user(@user)
+
+  end#register
+
+  def type_menu(type_user)
+    if type_user == "client"
+      @view.products(@products, type_user)
+
+    elsif type_user == "vendor"
+      op = @view.menu_vendor(type_user)
+      if op == "1"
+        @view.products(@products, type_user)
+      else
+        sell(type_user)
+      end#if vendor
+
+    else 
+
+    end#if
+        
+
+  end#type_menu
+
+  def sell(type_user)
+    aux = @view.sell
+    product = @csv.read_products
+
+  end#sell
+
+
+  # def register   
+  #   op = gets.chomp 
+  #   case op
+  #   when "1" then 
+  #   when "2" then 
+  # end
 
 end#class OnlineStore
 
 OnlineStore.new
+
+
+
